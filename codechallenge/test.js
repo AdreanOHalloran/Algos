7ca09e5a10bd618b9b4fd1d8d675cbe4c290bd27
Adrean1983 <adrean_@hotmail.com>
2020-07-05 10:16:38 -0400

    add array and object reassignment tasks

---
+++ codechallenge/test.js
@@ -0,0 +1,392 @@
+// fizzbuzz - With modulus https://www.youtube.com/watch?v=QPZ0pIK_wsc
+
+// -------------------------------------------------------------------------------------------------------
+
+// fizzbuzz - without modulus
+
+// -------------------------------------------------------------------------------------------------------
+
+// // Reverse a string -- FreeCodeCamp
+// // js Cardio https://www.youtube.com/watch?v=M2bJBuaOeOQ
+// function reverseString(str) {
+// // using methods with variables
+
+// // using methods
+
+// //manually using forloop Decrement
+
+// // forLoop Increment
+
+// // for of loop
+
+// // forEach
+
+// }
+// console.log(reverseString("hello")); // olleh
+
+// -------------------------------------------------------------------------------------------------------
+
+// Add numbers in an array -- HackerRank
+// function simpleArraySum(ar) {
+
+// }
+// console.log(simpleArraySum([1, 2, 3, 4, 10, 11]));
+
+// -------------------------------------------------------------------------------------------------------
+
+// Comparing results between two people -- HackeRank https://www.hackerrank.com/challenges/compare-the-triplets/problem
+// function compareTriplets(a, b) {
+
+// }
+// console.log(compareTriplets([17, 28, 30], [99, 16, 8]));
+
+// -------------------------------------------------------------------------------------------------------
+
+// // Filter an array  -- integers
+// var arr3 = [5, 3, 100, 55, 89, 300];
+
+// console.log(newArr3);
+// console.log(arr3.includes(100));
+
+// -------------------------------------------------------------------------------------------------------
+
+// //Sorting an array "strings"
+// var arr2 = ['a', 'abc', 'tr', 'bcd'];
+
+// console.log(sortedArr);
+
+// -------------------------------------------------------------------------------------------------------
+
+// // Sorting an array integers
+// var arr1 = [4, 230, 1, 60, 66, 153543, 13264356864323];
+
+// console.log(sortArr);
+
+// -------------------------------------------------------------------------------------------------------
+
+// factoralize 5! = 1 * 2 * 3 * 4 * 5 = 120 -- Freecodecamp https://www.youtube.com/watch?v=4ADnpDUQFvc&index=2&list=PLkBfv4fGBau-q9tGBUgVoGClRsSBiiZm8
+// function factorialize(num) {
+
+// }
+// console.log(factorialize(5));
+
+// -------------------------------------------------------------------------------------------------------
+
+// //Palindrome -- Freecodecamp https://www.youtube.com/watch?v=hvV48xfwZCs
+// let palindrome = (str) => {
+//     let reg = /[\W_]/g;
+
+//     let smallStr = str.toLowerCase().replace(reg, "");
+
+// }
+// palindrome("racecar");
+// palindrome("dog");
+
+// -------------------------------------------------------------------------------------------------------
+
+// Find Longest Word -- Freecodecamp https://www.youtube.com/watch?v=dy4y5MFVjbs&list=PLkBfv4fGBau-q9tGBUgVoGClRsSBiiZm8&index=4
+// let findLongestWord = (str) => {
+//     // using array methods
+
+//     // using for of loop
+
+//     console.log(longest);
+//     console.log(longest.length);
+// }
+// findLongestWord("I want to find out what is the longest word in this sentence")
+
+// -------------------------------------------------------------------------------------------------------
+
+// Capitalize the first letter of each word (titleCase) ---FreeCodeCamp https://www.youtube.com/watch?v=lzs29obgJKM
+function titleCase(str) {
+  // // First Way for loop
+  // let words = str.split(' ');
+
+  // for (let i = 0; i < words.length; i++) {
+  //   words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+  // }
+
+  // console.log(words.join(' '));
+
+  //     //Second Way --- Map Method
+
+  let titled = str.split(' ').map((word) => {
+    return word[0].toUpperCase() + word.slice(1);
+  });
+
+  console.log(titled.join(' '));
+}
+titleCase("I'm a little tea pot");
+
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+
+// Confirm Ending -- freecodecamp https://www.youtube.com/watch?v=0NJ6kPBn8dM
+// function confirmEnding(str, target) {
+
+// //     // 1st Method using endsWith method
+
+// //     //1st method simplified one line
+
+// //     //2nd method .substr
+
+// //     //2nd method .substr simplified
+
+// //     //slice method
+
+// }
+// console.log(confirmEnding("Bastion", "on"));
+// console.log(confirmEnding("Bastion", "tn"));
+
+// -------------------------------------------------------------------------------------------------------
+
+// // Repeat a  string https://www.youtube.com/watch?v=f0yEu6-Di10
+// function repeatStringNumTimes(str, num) {
+//     // // with return() method with repeat
+
+//     //     // with for loop
+
+// }
+
+// console.log(repeatStringNumTimes("abc", 10));
+
+// -------------------------------------------------------------------------------------------------------
+
+//truncate a sting https://www.youtube.com/watch?v=UXw_UfxY-no
+// function truncateString(str, num) {
+
+// }
+// console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
+
+// -------------------------------------------------------------------------------------------------------
+
+// break array into groups - Chunky Monkey - Freecodecamp https://www.youtube.com/watch?v=jBlnlSNUdHo
+// function chunkArrayInGroups(arr, size) {
+//     // slice method (while loop)
+
+// }
+// console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+
+// -------------------------------------------------------------------------------------------------------
+
+//  // slasher flick --- Splice Method https://www.youtube.com/watch?v=Qc7iwtL1ISo
+// function slasher(arr, howMany) {
+
+// }
+// console.log(slasher([1,2,3], 2));
+// console.log(slasher(["burgers","fries","shake"], 1));
+
+// -------------------------------------------------------------------------------------------------------
+
+// const convert = (temp, unit) => {
+
+// }
+// convert(12, "C");
+// convert(32, "C");
+// convert(65, "C");
+// convert(100, "F");
+
+// -------------------------------------------------------------------------------------------------------
+
+// Determine the distance between the minute hand and hour hand.
+// function clock_angles(hour, minute) {
+// //     // first workout the minute angle
+
+// //     // then work out hour angle
+// //     // first the hour then the hour + mins
+
+// //         console.log("minute angle = " + minAngle);
+// //         console.log("hour angle = " + hourAngle);
+// //     //if minute angle greater than hour angle
+// //     // else if minute angle less than minute angle
+
+// }
+// console.log(clock_angles(3,15));
+
+// -------------------------------------------------------------------------------------------------------
+
+// JavaScript Cardio 3 - https://www.youtube.com/watch?v=tcoiPHktCwQ&feature=youtu.be
+
+// ADD ALL NUMBERS - no Arrays - Arguments object & for loop
+// function addAll() {
+// //solution 1 ES5 with 'arguments'
+
+//solution 2 ES6 - ...rest & reduce/forEach
+// function addAll(...numbers) {
+
+// }
+// console.log(addAll(2,5,6,7));
+
+// -------------------------------------------------------------------------------------------------------
+
+// // SUM ALL PRIMES https://www.youtube.com/watch?v=tcoiPHktCwQ&feature=youtu.be
+// function sumAllPrimes(num) {
+
+// }
+// console.log(sumAllPrimes(10));
+
+// -------------------------------------------------------------------------------------------------------
+
+// Seek and Destroy. Remove from Array Arguments https://www.youtube.com/watch?v=tcoiPHktCwQ&feature=youtu.be
+// function seekAndDestroy (arr) {
+// //solution 1: arguments, indexOf, filter
+
+//     // Return true if NOT in array
+
+// }
+
+// return arr.filter(filterArr);
+
+//Solution 2: ..rest, filter, includes
+// function seekAndDestroy (arr, ...rest) {
+
+// }
+// console.log(seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6));
+
+// -------------------------------------------------------------------------------------------------------
+
+// // SORT BY HEIGHT [-1, 150, 190, 170, -1, -1, 160, 180] https://www.youtube.com/watch?v=LHn8WEj3CfU
+// function sortByHeight (a) {
+//     const arr1 = [];
+//     const arr2 = [];
+
+// }
+// const a = [-1, 150, 190, 170, -1, -1, 160, 180]
+// console.log(sortByHeight(a));
+
+// -------------------------------------------------------------------------------------------------------
+
+// // MISSING LETTERS
+// function missingLetters(str) {
+
+// }
+// console.log(missingLetters("abce"));
+
+// -------------------------------------------------------------------------------------------------------
+
+// // EVEN & ODD SUMS. sum the even and the odd numbers separately from an array.
+// function evenOddSums(arr) {
+//     let evenSum = 0;
+//     let oddSum = 0;
+
+//     return [evenSum, oddSum];
+// }
+// console.log(evenOddSums([50, 60, 60, 45, 71]));
+
+// -------------------------------------------------------------------------------------------------------
+
+// Mutations - FreeCodeCamp.check if word contains letters https://www.youtube.com/watch?v=WGTVz35ZTaw&index=13&list=PLkBfv4fGBau-q9tGBUgVoGClRsSBiiZm8&t=0s
+// function mutation(arr) {
+//     var firstWord = arr[0].toLowerCase();
+//     var secondWord = arr[1].toLowerCase();
+
+// first method for loop
+
+//     // second method for of with index
+
+//      // Third Method - Includes
+//
+//
+//     return true;
+// }
+// console.log(mutation(["hello", "hey"]));
+// console.log(mutation(["hello", "he"]));
+
+// -------------------------------------------------------------------------------------------------------
+
+// // Freecodecamp Falsy Bouncer- Remove all false things from an array  https://www.youtube.com/watch?v=GdU0oQWxTB4&list=PLkBfv4fGBau-q9tGBUgVoGClRsSBiiZm8&index=13
+// // solution 1; for of loop
+// function bouncer(arr) {
+//     var truthies = [];
+
+//     return truthies;
+// }
+// // solution 2: filter
+// function bouncer(arr) {
+
+// }
+// console.log(bouncer([7, "ate", "", false, 9]));
+
+// -------------------------------------------------------------------------------------------------------
+
+// // Reverse an integer
+// function reverseInt(int) {
+
+// }
+// console.log(reverseInt(54321));
+
+// -------------------------------------------------------------------------------------------------------
+
+// // 10 times tables -- for loop
+
+// -------------------------------------------------------------------------------------------------------
+
+// Where do I belong - Freecodecamp
+// https://www.youtube.com/watch?v=MKKBqpEif9M&list=PLkBfv4fGBau-q9tGBUgVoGClRsSBiiZm8&index=15
+// function getIndexToIns(arr, num) {
+// // sort then for loop
+
+// // solution 2 https://www.youtube.com/watch?v=Xz2BqAuW3Gw
+
+// }b
+// console.log(getIndexToIns([40, 60], 50));
+
+// -------------------------------------------------------------------------------------------------------
+
+// Ceasers Cipher - Freecodecamp
+
+// function rot13(str) { // LBH QVQ VG!
+//     //solution one for loop with if statments (no regex or array methods)
+//     // https://www.youtube.com/watch?v=4mp-6a3vARU&list=PLkBfv4fGBau-q9tGBUgVoGClRsSBiiZm8&index=16
+
+// // https://www.youtube.com/watch?v=eAe0Xo57AKM
+// // Solution 2 - map & regex solution.
+
+// }
+// console.log(rot13("SERR PBQR PNZC"));
+
+// -------------------------------------------------------------------------------------------------------
+
+// // Get how many pairs of numbers. Don't include left over or single socks
+// function sockMerchant(n, ar) {
+
+// }
+// console.log(sockMerchant(9, ["10", "20", "20", "10", "10", "30", "50", "10", "20"]))
+
+// -------------------------------------------------------------------------------------------------------
+
+// There are four ways to copy an array without making a direct reference to the original array
+// Bad: const players = ['Tom', 'John', 'Mike', 'Harry']
+// const team = players
+// team[3] = 'Stanley' <--- this will update the original array of players
+
+// -------------------------------------------------------------------------------------------------------
+
+// Object Reassignment. There are two ways to do this safetly
+// const person = {
+//   name: 'Adrean',
+//   age: 36,
+// };
+// // Bad - this reassigns the original
+// const man = person;
+// man.age = 99;
+
+// Deep Clone an object
+// const adrean = {
+//   name: 'adrean',
+//   age: 36,
+//   social = {
+//     facebook: '@facebook',
+//     twitter: '@twitter'
+//   }
+// }
+// Bad
+// const dev = {...adrean};
+// dev.name = 'Age';
+// dev.social.twitter = 'coolman'
+// console.log(adrean);
+
+// Write Hacky way to do a deep clone. This makes it a string and then back into an object
+
+// -------------------------------------------------------------------------------------------------------
